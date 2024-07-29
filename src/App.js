a18a2eb7d8c057094b67eefed473e62a83cdb04f
Nicolette Steenkamp <steenkampnicolette815@gmail.com>
2024-07-29 15:52:57 +0200

    Added a footer and created Dictionary component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import logo from "./logo.png"
+import logo from "./logo.png";
+import Dictionary from "./Dictionary";
 import './App.css';
 
  export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <img src={logo} className="app-logo img-fluid" alt="logo"/>
-       
-        
-      </header>
+       </header>
+       <main>
+        <Dictionary/>
+        </main>
+      <footer className="app-footer">Coded by Nicolette Steenkamp, the code is <a href="https://github.com/NicoletteSteenkamp/dictionary" target="_blank" rel="noreferrer"> open-sourced</a> and deployed on <a href="https://dictionarify.netlify.app/" target="_blank" rel="noreferrer">Netlify</a></footer>
+      </div>
     </div>
   );
 }
