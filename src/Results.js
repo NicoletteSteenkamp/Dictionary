94be170aa46465845b6d827f49af0442a27ad931
Nicolette Steenkamp <steenkampnicolette815@gmail.com>
2024-08-02 12:48:38 +0200

    created the different components

--- src/Results.js
+++ src/Results.js
@@ -1,10 +1,16 @@
+import React from "react";
+import Meaning from "./Meaning";
+
 export default function Results(props) {
     if (props.results.length > 0) {
       return (
-        <div className="Results">
-          <p>{props.results}</p>
-        </div>
-      );
+        <div className="Result">
+        <h2>{props.result.word}</h2>
+        <h5>/{props.result.phonetic}/</h5>
+        <Meaning meaning={props.result.meanings} />
+      </div>
+    );
+    
     } else {
       return null;
     }
