551e69ae16c9b626e3ec427ee710b644b728e0ca
Nicolette Steenkamp <steenkampnicolette815@gmail.com>
2024-08-11 16:19:30 +0200

    styled some more

--- src/Meaning.js
+++ src/Meaning.js
@@ -5,16 +5,17 @@ import "./Meaning.css";
 export default function Meaning(props) {
   return (
     <div className="Meaning">
+      <section>
       <h3>{props.meaning.partOfSpeech}</h3>
-      <div>
+    
         <div className="definition">{props.meaning.definition}</div>
 
         <div className="example">{props.meaning.example}</div>
 
         <Synonyms synonyms={props.meaning.synonyms} />
-
+        </section>
       </div>
-    </div>
+  
   );
 }
   
--- src/Phonetic.js
+++ src/Phonetic.js
@@ -2,5 +2,17 @@ import React from "react";
 import "./Phonetic.css";
 
 export default function Phonetic(props) {
-  return <h4>Phonetic: {props.phonetic}</h4>;
+  return (
+    <div>
+      {props.phonetic ? (
+        <section>
+          <h4>Phonetic: {props.phonetic}</h4>
+        </section>
+      ) : (
+        <section>
+          <h4>Phonetic information is not available</h4>
+        </section>
+      )}
+    </div>
+  );
 }
